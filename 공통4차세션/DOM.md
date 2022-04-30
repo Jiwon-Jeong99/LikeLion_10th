4896f9b25f749cd4cb1ea9e7b8e069481736e3a5
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-04-30 16:02:20 +0900

    딕셔너리 리스트

--- 공통4차세션/DOM.md
+++ 공통4차세션/DOM.md
@@ -5,3 +5,6 @@ Rendering : 화면 상에 엘리먼트들을 보여주는 과정
 DOM Element
 
 font-size를 js에서 바꾸고 싶으면, fontSize라고 명명한다.
+
+1. css 속성을 바꾸고 싶으면 -> style.
+2. 내용을 바꾸고 싶으면 -> innerHTML
--- 파이썬세션/기초.py
+++ 파이썬세션/기초.py
@@ -1,5 +1,55 @@
-# 리스트 - 순서대로 차곡차곡
+# # 리스트 - 순서대로 차곡차곡
 item_list = ["지원", "민서", "동길", "해윤", "교현"]
-print(item_list[0])
 
-# 딕셔너리 - 
+# new_data = []
+
+# for x in item_list:
+#     if x == "명준":
+#         new_data.append(x)
+
+num_list = [1,2,3,4,5,6,7,8,9,10]
+
+new_data = []
+for n in num_list:
+    if n%2 == 0:
+        new_data.append(n)
+print(new_data)
+
+# print(item_list[0])
+
+# # 리스트 접근
+# a = item_list[3]
+
+# # 리스트 값 지정
+# item_list[0] = "지원"
+
+# # 리스트 메서드
+# item_list.append("명준")
+# print(item_list)
+
+
+# # 딕셔너리 
+# item_dic = {"key1" : 111, "key2" : 222}
+# print(item_dic)
+
+# # 딕셔너리 접근
+# print(item_dic["key1"])
+
+# # 딕셔너리 변경
+# item_dic["key1"] = 222
+# print(item_dic)
+
+# # 딕셔너리 추가
+# item_dic["key3"] = 333
+# print(item_dic)
+
+# # 딕셔너리 메서드
+# # 딕셔너리는 키값이나 value값만 받아서 리스트를 만들어서 for문 돌림
+# dic_val = item_dic.values()
+# print(dic_val)
+
+# dic_keys = item_dic.keys()
+# print(dic_keys)
+
+# dic_items = item_dic.items()
+# print(dic_items)
